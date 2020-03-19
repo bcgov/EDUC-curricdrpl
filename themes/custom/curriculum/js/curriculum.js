d5557ab78d190c13d9b2b00492b5eb50d6010988
Shaun Lum <shaun.lum@gov.bc.ca>
2020-03-19 14:56:05 -0700

    fix curriculum page to be responsive

--- themes/custom/curriculum/js/curriculum.js
+++ themes/custom/curriculum/js/curriculum.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.view-display-id-curriculum_course > .view-content .view-grouping:nth-child(1)').addClass("col-12 curriculum_big_ideas");
+    $('.view-display-id-curriculum_course > .view-content .view-grouping:nth-child(1) .curriculum-content').addClass("row");
     $('.view-display-id-curriculum_course > .view-content .view-grouping:nth-child(2)').addClass("col-12 col-md-5 curriculum_content");
     $('.view-display-id-curriculum_course > .view-content .view-grouping:nth-child(3)').addClass("col-12 col-md-7 curriculum_competencies");
-
+    
 
     /* curricular Competency Group as an elaboration */
 
